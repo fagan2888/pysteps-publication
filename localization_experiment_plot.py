aba0f4f1fca7e3ddfabfea3b2361021f6136eb13
ned <daniele.nerini@meteoswiss.ch>
2019-01-30 17:39:40 +0100

    Major revision of the script, using Seppo's code as basis.

---
+++ localization_experiment_plot.py
@@ -0,0 +1,183 @@
+"""
+Section 6.3
+This experiment investigates the impact of localization on the quality  of  the  
+nowcast.  For  localization  we  intend  the  use of  a  subset  of  the  
+observations  in  order  to  estimate  model parameters that are distributed in 
+space. The short-space approach used in Nerini et al. (2017) is generalized to 
+the whole nowcasting system. This essenially boils down to a moving window 
+localization of the nowcasting procedure, whereby all parameters are estimated 
+over a subdomain of prescribed size.
+
+This script plots the results.
+"""
+
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+from pylab import *
+import pickle
+from pysteps.verification import ensscores, probscores
+
+linecolors = ["C0", "C1", "C2", "C3", "C4", "C5", "C6"]
+linestyles = ['-', '-', '-', '-', '-']
+markers = ['o', 'o', 'o', 'o', 'o']
+minleadtime = 10
+maxleadtime = 60
+fixedleadtimes = [5, 11] # timesteps
+minmaxleadtimes = [10, 60] # min
+R_thrs = [0.1, 1.0, 5.0, 10.0]
+
+with open("window_size_results.dat", "rb") as f:
+    results = pickle.load(f)
+    
+for R_thr in R_thrs:
+        
+    # ROC curves
+    for lt in fixedleadtimes:
+
+        figure()
+
+        plot([0, 1], [0, 1], "k--")
+
+        for i,ws in enumerate(results.keys()):
+            ROC = results[ws]["ROC"][R_thr][lt]
+            POFD,POD,area = probscores.ROC_curve_compute(ROC, compute_area=True)
+            plot(POFD, POD, color=linecolors[i], linestyle='-', marker='D', 
+                 label="%d km (area=%.3f)" % (ws, area))
+
+        xlim(0, 1)
+        ylim(0, 1)
+        xlabel("False alarm rate (POFD)")
+        ylabel("Probability of detection (POD)")
+        grid(True, ls=':')
+        legend(fontsize=12)
+
+        savefig("figures/window_size_ROC_curves_%dmin_%03.1fmm.pdf" % ((lt+1)*5, R_thr), bbox_inches="tight")
+        
+    # Reliability diagrams
+
+    for lt in fixedleadtimes:
+        fig = figure()
+        ax = fig.gca()
+        iax = inset_axes(ax, width="35%", height="20%", loc=4, borderpad=3.5)
+
+        sample_size_min = []
+        sample_size_max = []
+
+        for i,ws in enumerate(results.keys()):
+            reldiag = results[ws]["reldiag"][R_thr][lt]
+
+            f = 1.0 * reldiag["Y_sum"] / reldiag["num_idx"]
+            r = 1.0 * reldiag["X_sum"] / reldiag["num_idx"]
+
+            mask = np.logical_and(np.isfinite(r), np.isfinite(f))
+
+            ax.plot(r[mask], f[mask], color=linecolors[i], linestyle='-', marker='D', 
+                    label="%d km" % ws)
+
+            bd = 0.5 * (reldiag["bin_edges"][1] - reldiag["bin_edges"][0])
+            iax.plot(reldiag["bin_edges"][:-1] + bd, reldiag["sample_size"], 
+                     color=linecolors[i], linestyle='-', marker='D', ms=3)
+
+            sample_size_min.append(int(max(floor(log10(min(reldiag["sample_size"]))), 1)))
+            sample_size_max.append(int(ceil(log10(max(reldiag["sample_size"])))))
+
+        iax.set_yscale("log", basey=10)
+        iax.set_xticks(reldiag["bin_edges"])
+        iax.set_xticklabels(["%.1f" % max(v, 1e-6) for v in reldiag["bin_edges"]])
+        yt_min = min(sample_size_min)
+        yt_max = max(sample_size_max)
+        t = [pow(10.0, k) for k in range(yt_min, yt_max)]
+        iax.set_yticks([int(t_) for t_ in t])
+        iax.set_xlim(0.0, 1.0)
+        iax.set_ylim(0.5*t[0], 5*t[-1])
+        iax.set_ylabel("$\log_{10}$(samples)")
+        iax.yaxis.tick_right()
+        iax.yaxis.set_label_position("right")
+        iax.tick_params(axis="both", which="major", labelsize=6)
+        iax.grid(axis='y', ls=':')
+
+        ax.plot([0, 1], [0, 1], "k--")
+
+        ax.set_xlim(0, 1)
+        ax.set_ylim(0, 1)
+        ax.grid(True, ls=':')
+        ax.legend(fontsize=12)
+        ax.set_xlabel("Forecast probability")
+        ax.set_ylabel("Observed relative frequency")
+
+        savefig("figures/window_size_reldiags_%dmin_%03.1fmm.pdf" % ((lt+1)*5, R_thr), bbox_inches="tight")
+        
+    # Rank hist
+
+    for lt in fixedleadtimes:
+        figure()
+
+        r_max = 0.0
+        for i,ws in enumerate(results.keys()):
+            rankhist = results[ws]["rankhist"][R_thr][lt]
+            r = ensscores.rankhist_compute(rankhist)
+            r_max = max(r_max, np.max(r))
+            x = np.linspace(0, 1, rankhist["num_ens_members"] + 1)
+            x += 0.5 * (x[1] - x[0])
+            plot(x, r, color=linecolors[i], linestyle='-', marker='D', 
+                 label="%d km" % ws)
+                 
+        plot(x, np.ones_like(x)*1/x.size, "k--", lw=0.8)
+
+        xticks(x[::3] + (x[1] - x[0]), np.arange(1, len(x))[::3])
+        xlim(0, 1+1.0/len(x))
+        ylim(0, r_max*1.25)
+        xlabel("Rank of observation (among ensemble members)")
+        ylabel("Relative frequency")
+        grid(True, axis='y', ls=':')
+        legend(fontsize=12)
+
+        savefig("figures/window_size_rankhists_%dmin_%03.1fmm.pdf" % ((lt+1)*5, R_thr), bbox_inches="tight")
+        
+        
+    # ROC areas
+
+    ROC_areas = dict([(ws, []) for ws in results.keys()])
+    OPs = dict([(ws, []) for ws in results.keys()])
+
+    for ws in results.keys():
+      if not ws in ROC_areas.keys():
+          ROC_areas[ws] = []
+      for lt in sorted(results[ws]["ROC"][R_thr].keys()):
+          a = probscores.ROC_curve_compute(results[ws]["ROC"][R_thr][lt], compute_area=True)[2]
+          ROC_areas[ws].append(a)
+      for lt in sorted(results[ws]["rankhist"][R_thr].keys()):
+          rh = ensscores.rankhist_compute(results[ws]["rankhist"][R_thr][lt])
+          OP = (rh[0] + rh[-1]) / sum(rh)
+          OPs[ws].append(OP) 
+
+    fig = figure()
+    ax = fig.gca()
+    for i,ws in enumerate(ROC_areas.keys()):
+        leadtimes = (arange(len(ROC_areas[ws])) + 1) * 5
+        ax.plot(leadtimes, ROC_areas[ws], ls=linestyles[i], marker=markers[i], 
+                color=linecolors[i], label="%d km" % ws, lw=2, ms=6)
+    ax.set_xlim(minmaxleadtimes[0], minmaxleadtimes[1])
+    # ax.set_ylim(0.82, 0.98)
+    ax.grid(True)
+    ax.legend(fontsize=12)
+    ax.set_xlabel("Lead time (minutes)", fontsize=12)
+    ax.set_ylabel("ROC area", fontsize=12)
+
+    fig.savefig("figures/window_size_ROC_areas_%03.1fmm.pdf" % R_thr, bbox_inches="tight")
+
+    # OP
+
+    fig = figure()
+    ax = fig.gca()
+    for i,ws in enumerate(OPs.keys()):
+        leadtimes = (arange(len(OPs[ws])) + 1) * 5
+        ax.plot(leadtimes, OPs[ws], ls=linestyles[i], marker=markers[i], 
+                color=linecolors[i], label="%d km" % ws, lw=2, ms=6)
+    ax.set_xlim(minmaxleadtimes[0], minmaxleadtimes[1])
+    # ax.set_ylim(0.01, 0.675)
+    ax.grid(True)
+    ax.legend(fontsize=12)
+    ax.set_xlabel("Lead time (minutes)", fontsize=12)
+    ax.set_ylabel("Percentage of outliers", fontsize=12)
+
+    fig.savefig("figures/window_size_OPs_%03.1fmm.pdf" % R_thr, bbox_inches="tight")
---
+++ localization_experiment_run.py
@@ -0,0 +1,166 @@
+# Runs SSEPS with different window sizes and computes probabilistic 
+# verification statistics with different intensity thresholds
+
+import dask
+from datetime import datetime, timedelta
+import numpy as np
+import os
+import pickle
+import sys
+import time
+from pysteps import io, motion, nowcasts, utils
+from pysteps.postprocessing.ensemblestats import excprob
+from pysteps.utils import transformation, conversion
+from pysteps.verification import ensscores
+from pysteps.verification import probscores
+import datasources, precipevents
+
+window_sizes = [710, 360, 180, 90]
+domain = "mch_rzc"
+timestep = 30
+num_workers = 24
+num_timesteps = 18
+ensemble_size = 24
+R_min = 0.1
+R_thrs = [0.1, 1.0, 5.0, 10.0]
+
+datasource = datasources.mch_rzc
+precipevents = precipevents.mch
+
+# vp_par  = (2.56338484, 0.3330941, -2.99714349) # mch only
+# vp_perp = (1.31204508, 0.3578426, -1.02499891)
+vp_par  = (2.31970635, 0.33734287, -2.64972861) # fmi+mch
+vp_perp = (1.90769947, 0.33446594, -2.06603662)
+
+root_path = os.path.join(datasources.root_path, datasource["root_path"])
+importer = io.get_method(datasource["importer"], "importer")
+
+# read Swiss radar data quality (list of timestamps with low-quality observations)
+badts2016 = np.loadtxt("/store/msrad/radar/precip_attractor/radar_availability/AQC-2016_radar-stats-badTimestamps_00005.txt", dtype="str")
+badts2017 = np.loadtxt("/store/msrad/radar/precip_attractor/radar_availability/AQC-2017_radar-stats-badTimestamps_00005.txt", dtype="str")
+badts = np.concatenate((badts2016, badts2017))
+
+results = {}
+
+for ws in window_sizes:
+    results[ws] = {}
+    results[ws]["reldiag"] = {}
+    results[ws]["rankhist"] = {}
+    results[ws]["ROC"] = {}
+    for R_thr in R_thrs:
+        results[ws]["reldiag"][R_thr] = {}
+        results[ws]["rankhist"][R_thr] = {}
+        results[ws]["ROC"][R_thr] = {}
+        for lt in range(num_timesteps):
+            results[ws]["reldiag"][R_thr][lt] = probscores.reldiag_init(R_thr, n_bins=10)
+            results[ws]["rankhist"][R_thr][lt] = ensscores.rankhist_init(ensemble_size, R_thr)
+            results[ws]["ROC"][R_thr][lt] = probscores.ROC_curve_init(R_thr, n_prob_thrs=100)
+
+for pei,pe in enumerate(precipevents):
+    curdate = datetime.strptime(pe[0], "%Y%m%d%H%M")
+    enddate = datetime.strptime(pe[1], "%Y%m%d%H%M")
+    
+    countnwc = 0
+    while curdate <= enddate:
+
+        if curdate + num_timesteps*timedelta(minutes=datasource["timestep"]) > enddate:
+            break
+        countnwc += 1    
+        print("Computing nowcast %d for event %d, start date %s..." % (countnwc, pei+1, str(curdate)))
+
+        fns = io.archive.find_by_date(curdate, root_path, datasource["path_fmt"],
+                                      datasource["fn_pattern"], datasource["fn_ext"],
+                                      datasource["timestep"], num_prev_files=3)
+
+        R,_,metadata = io.readers.read_timeseries(fns, importer,
+                                                  **datasource["importer_kwargs"])
+
+        missing_data = False
+        for i in range(R.shape[0]):
+            if not np.any(np.isfinite(R[i, :, :])):
+                print("Skipping, no finite values found for time step %d" % (i+1))
+                missing_data = True
+                break
+                
+        low_quality = False
+        for timestamp in metadata["timestamps"]:
+            if timestamp.strftime("%Y%m%d%H%M") in badts:
+                print("Skipping, low quality observation found for time step %d" % (i+1))
+                low_quality = True
+                break
+
+        if missing_data or low_quality:
+            curdate += timedelta(minutes=timestep)
+            continue
+
+        R[~np.isfinite(R)] = metadata["zerovalue"]
+        R, metadata = conversion.to_rainrate(R, metadata)
+        R, metadata = transformation.dB_transform(R, metadata, threshold=R_min)
+
+        obs_fns = io.archive.find_by_date(curdate, root_path, datasource["path_fmt"],
+                                          datasource["fn_pattern"], datasource["fn_ext"],
+                                          datasource["timestep"], 
+                                          num_next_files=num_timesteps)
+        obs_fns = (obs_fns[0][1:], obs_fns[1][1:])
+        if len(obs_fns[0]) == 0:
+            curdate += timedelta(minutes=timestep)
+            print("Skipping, no verifying observations found.")
+            continue
+
+        R_obs,_,metaobs = io.readers.read_timeseries(obs_fns, importer,
+                                                 **datasource["importer_kwargs"])
+        R_obs, metaobs = conversion.to_rainrate(R_obs, metaobs)
+
+        if R_obs is None:
+            curdate += timedelta(minutes=timestep)
+            print("Skipping, no verifying observations found.")
+            continue
+
+        for ws in window_sizes:
+            oflow = motion.get_method("lucaskanade")
+            V = oflow(R)
+            nc = nowcasts.sseps.forecast
+            vel_pert_kwargs = {"p_pert_par":vp_par , "p_pert_perp":vp_perp}
+            R_fct = nc(R[-3:, :, :], metadata, V, num_timesteps, 
+                       n_ens_members=ensemble_size, n_cascade_levels=6, 
+                       win_size=ws, overlap=0.1,
+                       vel_pert_method="bps", 
+                       mask_method="incremental", num_workers=num_workers, 
+                       fft_method="numpy", vel_pert_kwargs=vel_pert_kwargs)
+
+            R_fct = transformation.dB_transform(R_fct, metadata, inverse=True)[0]
+
+            def worker(lt, R_thr):
+                if not np.any(np.isfinite(R_obs[lt, :, :])):
+                    return
+
+                P_fct = excprob(R_fct[:, lt, :, :], R_thr)
+
+                probscores.reldiag_accum(results[ws]["reldiag"][R_thr][lt], 
+                                         P_fct, R_obs[lt, :, :])
+                ensscores.rankhist_accum(results[ws]["rankhist"][R_thr][lt], 
+                                         R_fct[:, lt, :, :], R_obs[lt, :, :])
+                probscores.ROC_curve_accum(results[ws]["ROC"][R_thr][lt], 
+                                           P_fct, R_obs[lt, :, :])
+
+            res = []
+            for lt in range(num_timesteps):
+                missing_data = np.all(~np.isfinite(R_obs[lt, :, :]))
+                if missing_data:
+                    print("Warning: no verifying observations for lead time %d." % (lt+1))
+                    continue
+                low_quality = metaobs["timestamps"][lt].strftime("%Y%m%d%H%M") in badts
+                if low_quality:
+                    print("Warning: low quality observations for lead time %d." % (lt+1))
+                    continue
+                for R_thr in R_thrs:
+                    res.append(dask.delayed(worker)(lt, R_thr))
+            dask.compute(*res, num_workers=num_workers)
+
+        with open("window_size_results_2.dat", "wb") as f:
+            pickle.dump(results, f)
+
+        curdate += timedelta(minutes=timestep)
+
+with open("window_size_results_2.dat", "wb") as f:
+    pickle.dump(results, f)
