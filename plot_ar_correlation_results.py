124cc178bd45925c1fb4bf91ef66e8d208592742
Seppo Pulkkinen <pulkkins@gmail.com>
2019-02-16 23:41:29 -0700

    Add frequency band numbers to the plot

--- plot_ar_correlation_results.py
+++ plot_ar_correlation_results.py
@@ -24,6 +24,15 @@ l = pyplot.legend([lines[0], lines[num_cascade_levels+1]], ["AR(2)", "Observed"]
                   fontsize=12, loc=(0.6, 0.9))
 ax.add_artist(l)
 
+ax.text(150, 0.85, '1', fontsize=10)
+ax.text(139, 0.71, '2', fontsize=10)
+ax.text(120, 0.35, '3', fontsize=10)
+ax.text(93, 0.18, '4', fontsize=10)
+ax.text(75, 0.07, '5', fontsize=10)
+ax.text(38, 0.05, '6', fontsize=10)
+ax.text(15, 0.05, '7', fontsize=10)
+ax.text(7, 0.021, '8', fontsize=10)
+
 xt = np.hstack([[5], np.arange(0, np.max(leadtimes)+5, 20)])
 ax.set_xticks(xt)
 ax.set_xticklabels([int(v) for v in xt])
