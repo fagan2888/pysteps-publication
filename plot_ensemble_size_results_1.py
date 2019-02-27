e5ed690b2c15ed48473a4723b22097f57c7c2361
Seppo Pulkkinen <pulkkins@gmail.com>
2019-02-27 14:31:42 -0700

    Plotting adjustments

--- plot_ensemble_size_results_1.py
+++ plot_ensemble_size_results_1.py
@@ -11,8 +11,8 @@ linestyles = ['-', '-', '-', '-', '-']
 #markers = ['o', 's', 'd', '^']
 markers = ['o', 'o', 'o', 'o', 'o']
 #markers = ["circle", "cross", "diamond", "square", "triangle-down", "triangle-up"]
-minleadtime = 10
-maxleadtime = 90
+minleadtime = 5
+maxleadtime = 180
 R_thr = 0.1
 
 with open("ensemble_size_results.dat", "rb") as f:
@@ -30,33 +30,37 @@ for es in sorted(results.keys()):
   for lt in sorted(results[es]["rankhist"][R_thr].keys()):
       rh = ensscores.rankhist_compute(results[es]["rankhist"][R_thr][lt])
       OP = (rh[0] + rh[-1]) / sum(rh)
-      OPs[es].append(OP) 
+      OPs[es].append(OP)
 
-fig = figure()
+fig = figure(figsize=(5, 3.5))
 ax = fig.gca()
 for i,es in enumerate(sorted(ROC_areas.keys())):
     leadtimes = (arange(len(ROC_areas[es])) + 1) * 5
     ax.plot(leadtimes, ROC_areas[es], ls=linestyles[i], marker=markers[i], 
             color=linecolors[i], label="n=%d" % es, lw=2, ms=6)
+xt = np.hstack([5, np.arange(20, maxleadtime+20, 20)])
+ax.set_xticks(xt)
 ax.set_xlim(minleadtime, maxleadtime)
-ax.set_ylim(0.82, 0.98)
+ax.set_ylim(0.7, 1.0)
 ax.grid(True)
-ax.legend(fontsize=12)
+ax.legend(fontsize=12, framealpha=1.0)
 ax.set_xlabel("Lead time (minutes)", fontsize=12)
 ax.set_ylabel("ROC area", fontsize=12)
 
 fig.savefig("ensemble_size_ROC_areas.pdf", bbox_inches="tight")
 
-fig = figure()
+fig = figure(figsize=(5, 3.5))
 ax = fig.gca()
 for i,es in enumerate(sorted(OPs.keys())):
     leadtimes = (arange(len(OPs[es])) + 1) * 5
     ax.plot(leadtimes, OPs[es], ls=linestyles[i], marker=markers[i], 
             color=linecolors[i], label="n=%d" % es, lw=2, ms=6)
+xt = np.hstack([5, np.arange(20, maxleadtime+20, 20)])
+ax.set_xticks(xt)
 ax.set_xlim(minleadtime, maxleadtime)
-ax.set_ylim(0.01, 0.675)
+ax.set_ylim(0.0, 0.4)
 ax.grid(True)
-ax.legend(fontsize=12)
+ax.legend(fontsize=12, framealpha=1.0, loc=(1.02, 0.3))
 ax.set_xlabel("Lead time (minutes)", fontsize=12)
 ax.set_ylabel("Percentage of outliers", fontsize=12)
 
